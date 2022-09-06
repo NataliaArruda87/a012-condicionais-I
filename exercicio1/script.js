7ffdee2ef768c1b54cec8747c9f329ca80b394de
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-09-06 16:21:20 -0300

    Resolução de exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,11 @@
-const num = //seu numero aqui
+//Crie um código que receba uma const numérica qualquer. 
+//Crie um if para verificar se o número guardado na const é par. 
+//Caso seja, imprima no console a mensagem: "o número é par".
 
-if(){
-    console.log("sua mensagem aqui");
+const num = prompt("Digite um numero:")
+
+if(num % 2 === 0){
+    console.log("É um numero par!");
+} else {
+    console.log("É um numero impar!");
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,36 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+/*Escreva um código que receba três valores. Uma idade, 
+um booleano que responda se a pessoa terminou ou não o ensino médio, 
+e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-if(){
+Crie um if para cada variável, checando as seguintes afirmacões:
 
+Se a pessoa tem 18 anos ou mais;
+Se a pessoa terminou o ensino médio;
+Se a pessoa NÃO está cursando alguma faculdade;
+
+Crie um if que imprima que a afirmacão é verdadeira, 
+e um else para imprimir que a afirmacão não é verdadeira. */
+
+
+
+let idade = prompt("Qual a sua idade?")
+let terminouColegial = confirm("Você já terminou o Ensino Médio?")
+let cursandoOutraFaculdade = confirm("Você não esta cursando alguma faculdade?")
+
+if(idade >= 18){
+    console.log("Maior de idade!")
 }else{
-	
+    console.log("Maior de idade!")
+}
+
+if(terminouColegial){
+    console.log("Terminou o ensino médio!")
+}else{
+    console.log("Ainda está cursando o ensino médio!")
+}
+
+if(!cursandoOutraFaculdade){
+    console.log("Não está cursando alguma faculdade!")
+}else{
+    console.log("Sim está cursando alguma faculdade!")
 }
